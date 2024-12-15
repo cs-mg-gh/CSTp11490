e7c7bde9d26af3219522355a42c6720236522a4c
Micah Heneveld <micahheneveld@gmail.com>
2024-12-14 17:31:15 -0800

    renamed and repurposed a few things.  More to do

--- flask/webFlask.py
+++ flask/webFlask.py
@@ -1,15 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap5
+from tcgdexsdk import TCGdex
 import requests
 
+tcgdex = TCGdex("en")
 
 # create an instance of Flask
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
 # Data storage for favorites
-favoritesN = []
-favorites = []
+checkedOutN = []
+checkedOut = []
 
 @app.route('/')
 def home():
@@ -18,27 +20,44 @@ def home():
 @app.route('/pokedex', methods=['GET'])
 def pokedex():
     pokemon_name = request.args.get('name', 'pikachu').lower()
-    add_to_favorites = request.args.get('favorite') == 'on'
 
     try:
         # Fetch Pokémon data
-        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_name}')
+        response = requests.get(f'https://api.tcgdex.net/v2/en/cards?name={pokemon_name}')
+
         response.raise_for_status()
         pokemon = response.json()
 
-        # Add to favorites if applicable
-        if add_to_favorites and pokemon_name not in favoritesN:
-            favoritesN.append(pokemon_name)
-            favorites.append(pokemon)
-
-        return render_template('pokedex.html', pokemon=pokemon, favorites=favorites, error=None)
+        return render_template('pokedex.html', pokemon=pokemon, checkedOut=checkedOut, error=None)
 
     except requests.exceptions.RequestException:
-        return render_template('pokedex.html', pokemon=None, favorites=favorites, error="Pokémon not found. Try another name!")
+        return render_template('pokedex.html', pokemon=None, checkedOut=checkedOut, error="Pokémon not found. Try another name!")
 
-@app.route('/favorites', methods=['GET'])
+@app.route('/cart', methods=['POST'])
+def cart():
+    card_id = request.form.get('card_id')
+
+    # Check if the card is already in the favorites list
+    if card_id and card_id not in [card['id'] for card in checkedOut]:
+        try:
+            # Fetch card details from the API
+            response = requests.get(f'https://api.tcgdex.net/v2/en/cards/{card_id}')
+            response.raise_for_status()
+            card = response.json()
+
+            # Add card to the favorites list
+            checkedOut.append(card)
+            checkedOutN.append(card['name'])
+
+
+        except requests.exceptions.RequestException:
+            return redirect(url_for('pokedex', error="Could not add card to favorites."))
+
+    return redirect(url_for('pokedex'))
+
+@app.route('/cart', methods=['GET'])
 def favorite_pokemon():
-    return render_template('favorites.html', favorites=favorites)
+    return render_template('cart.html', checkedOut=checkedOut)
 
 @app.route('/tierlist', methods=['GET'])
 def tier_list():
@@ -78,6 +97,22 @@ def tier_list():
 
     except requests.exceptions.RequestException:
         return "Error fetching Pokémon data.", 500
+    
+@app.route('/browseBySet', methods=['GET'])
+def browse():
+    set_name = request.args.get('set', 'Darkness Ablaze').lower()
+
+    try:
+        # Fetch Pokémon set data
+        response = requests.get(f'https://api.tcgdex.net/v2/en/sets/{set_name}')
+        response.raise_for_status()
+        pokemon_set = response.json()
+
+        return render_template('browseBySet.html', pokemon_set=pokemon_set, checkedOut=checkedOut, error=None)
+
+    except requests.exceptions.RequestException:
+        return render_template('browseBySet.html', pokemon_set=None, checkedOut=checkedOut, error="Set not found. Try another name!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
